3d3f7fcbf5a8a770fba8d18d662179ff1ba29120
Alex Bäuerle <alex@a13x.io>
2023-08-17 13:55:46 +0200

    fix: fix UI issues with chatbot data

--- frontend/src/lib/components/instance-views/views/views/OpenAIChat.svelte
+++ frontend/src/lib/components/instance-views/views/views/OpenAIChat.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import { authToken } from '$lib/stores';
+	import CircularProgress from '@smui/circular-progress/src/CircularProgress.svelte';
 	import AssistantBlock from './openai-chat/AssistantBlock.svelte';
 	import SystemBlock from './openai-chat/SystemBlock.svelte';
 	import UserBlock from './openai-chat/UserBlock.svelte';
@@ -6,9 +8,17 @@
 	export let entry: Record<string, number | string | boolean | { role: string; content: string }[]>;
 	export let modelColumn: string;
 
-	$: chatData = entry['data'] as { role: string; content: string }[];
-	$: showall = chatData.length <= 5;
-	$: entries = showall ? chatData : chatData.slice(-4);
+	$: showall = false;
+	$: fetchJSON = (async () => {
+		const response = await fetch(entry['data'] as string, {
+			headers: {
+				Authorization: 'Bearer ' + $authToken
+			}
+		});
+		const resp = await response.json();
+		showall = resp.length <= 5;
+		return resp;
+	})();
 
 	function entryString(
 		value: number | string | boolean | { role: string; content: string }[]
@@ -17,36 +27,41 @@
 	}
 </script>
 
-<div class="flex flex-col border border-grey-light rounded p-2.5 m-1">
-	{#if !showall}
-		<button
-			class="self-center bg-transparent cursor-pointer flex items-center p-1 -mt-1.5 rounded-2xl hover:bg-grey-lighter"
-			on:click={() => (showall = true)}
-		>
-			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 fill-grey-darker">
-				<path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
-			</svg>
-			<span class="pr-1">Show All</span>
-		</button>
-	{/if}
-	{#if chatData}
-		{#each entries as item}
-			{#if item['role'] === 'system'}
-				<SystemBlock input={item['content']} />
-			{:else if item['role'] === 'assistant'}
-				<AssistantBlock input={item['content']} />
-			{:else if item['role'] === 'user'}
-				<UserBlock input={item['content']} />
-			{/if}
-		{/each}
-	{/if}
-	{#if entry[modelColumn]}
-		<AssistantBlock input={entryString(entry[modelColumn])} output={true} />
-	{/if}
-	{#if entry['label']}
-		<div class="flex flex-col -mx-2.5 -mb-2.5 mt-2.5 p-1 border-t border-grey-lighter">
-			<span class="font-medium">Expected:</span>
-			<span>{entry['label']}</span>
-		</div>
-	{/if}
-</div>
+{#await fetchJSON}
+	<CircularProgress style="height: 32px; width: 32px; margin-right:20px" indeterminate />
+{:then data}
+	{@const entries = showall ? data : data.slice(-4)}
+	<div class="flex flex-col border border-grey-light rounded p-2.5 m-1">
+		{#if !showall}
+			<button
+				class="self-center bg-transparent cursor-pointer flex items-center p-1 -mt-1.5 rounded-2xl hover:bg-grey-lighter"
+				on:click={() => (showall = true)}
+			>
+				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 fill-grey-darker">
+					<path d="m12 8-6 6 1.41 1.41L12 10.83l4.59 4.58L18 14z" />
+				</svg>
+				<span class="pr-1">Show All</span>
+			</button>
+		{/if}
+		{#if data}
+			{#each entries as item}
+				{#if item['role'] === 'system'}
+					<SystemBlock input={item['content']} />
+				{:else if item['role'] === 'assistant'}
+					<AssistantBlock input={item['content']} />
+				{:else if item['role'] === 'user'}
+					<UserBlock input={item['content']} />
+				{/if}
+			{/each}
+		{/if}
+		{#if entry[modelColumn]}
+			<AssistantBlock input={entryString(entry[modelColumn])} output={true} />
+		{/if}
+		{#if entry['label']}
+			<div class="flex flex-col -mx-2.5 -mb-2.5 mt-2.5 p-1 border-t border-grey-lighter">
+				<span class="font-medium">Expected:</span>
+				<span>{entry['label']}</span>
+			</div>
+		{/if}
+	</div>
+{/await}
