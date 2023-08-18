6954ec327b2b45e6e8efba74c18bda45020f29c5
Alex Cabrera <alex.cabrera@gmail.com>
2023-08-18 07:09:10 -0700

    feat: update styles of start pages (#74)
    * feat: update styles of start pages
    
    * modules

--- frontend/src/routes/(user)/signup/+page.svelte
+++ frontend/src/routes/(user)/signup/+page.svelte
@@ -17,29 +17,41 @@
 	use:enhance
 >
 	<div class="flex flex-col items-center">
-		<Textfield input$name="username" value={form ? `${form.name}` : ''} label="User name" />
+		<a href="/">
+			<img src="/zeno-logo.png" alt="Zeno logo" width="200px" class="mb-5" />
+		</a>
+
+		<Textfield
+			input$name="username"
+			value={form ? `${form.name}` : ''}
+			label="User name"
+			class="w-56 mb-3"
+		/>
 		<Textfield
 			input$name="email"
 			type="email"
 			value={form ? `${form.email}` : ''}
 			label="Email address"
+			class="w-56 mb-3"
 		/>
 		<Textfield
 			input$name="password"
 			type="password"
 			value={form ? `${form.password}` : ''}
 			label="Password"
+			class="w-56 mb-3"
 		/>
 		<Textfield
 			input$name="repeatPassword"
 			type="password"
 			value={form ? `${form.repeat}` : ''}
 			label="Repeat password"
+			class="w-56 mb-3"
 		/>
-		<Button type="submit">Sign Up</Button>
-		<div>
-			Already have an account? <a href="/login">Log in now!</a>
-		</div>
+		<Button type="submit" variant="raised" class="mt-5">Sign Up</Button>
+		<p class="mt-5">
+			Already have an account? <a href="/">Log in now!</a>
+		</p>
 		{#if form?.error}
 			<p>{form.error}</p>
 		{/if}
