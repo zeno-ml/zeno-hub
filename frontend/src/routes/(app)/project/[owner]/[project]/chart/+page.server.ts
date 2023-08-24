32e87fb5bff32e10ef5c5245674b9901a4d14b80
Alex Bäuerle <alex@a13x.io>
2023-08-24 17:27:15 +0200

    fix: correct chart fetching with project uuid (#103)
    Since the project parameter changed with our change of referencing projects as owner/project, charts now need to first fetch the project uuid before being fetched.

--- frontend/src/routes/(app)/project/[owner]/[project]/chart/+page.server.ts
+++ frontend/src/routes/(app)/project/[owner]/[project]/chart/+page.server.ts
@@ -4,7 +4,8 @@ import { error } from '@sveltejs/kit';
 
 export async function load({ params }) {
 	OpenAPI.BASE = env.PUBLIC_BACKEND_ENDPOINT + '/api';
-	const charts = await ZenoService.getCharts(params.project);
+	const uuid = await ZenoService.getProjectUuid(params.owner, params.project);
+	const charts = await ZenoService.getCharts(uuid);
 	if (!charts) {
 		throw error(404, 'Could not load charts');
 	}
