408b41a5d4f15f5abc0cee556cd9ffc9f0caf12c
Alex Cabrera <alex.cabrera@gmail.com>
2023-08-24 16:29:41 -0700

    fix: local fetch in docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,12 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	server: {
 		proxy: {
+			'/dockerzeno': {
+				target: 'http://backend:8000',
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/dockerzeno/, '')
+			},
 			'/localzeno': {
 				target: 'http://127.0.0.1:8000',
 				changeOrigin: true,
