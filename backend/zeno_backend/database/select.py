85e596bb5457b37f7445629566a09982c49ba46b
Alex Cabrera <alex.cabrera@gmail.com>
2023-08-24 17:18:09 -0700

    fix: Fix repeat projects, tag size display, etc.

--- backend/zeno_backend/database/select.py
+++ backend/zeno_backend/database/select.py
@@ -143,6 +143,7 @@ def projects(user: User) -> list[Project]:
                 org_projects,
             )
         )
+        print(own_projects, user_projects, org_projects)
         return own_projects + user_projects + org_projects
 
 
