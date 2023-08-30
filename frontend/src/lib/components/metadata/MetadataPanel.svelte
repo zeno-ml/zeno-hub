970c52863c706bfe105c3dfd68ca4b31b85778d7
Alex Cabrera <alex.cabrera@gmail.com>
2023-08-30 08:42:13 -0700

    feat: collapsible sidebar (#131)
    * feat: collapsible sidebar
    
    * when to show, transition

--- frontend/src/lib/components/metadata/MetadataPanel.svelte
+++ frontend/src/lib/components/metadata/MetadataPanel.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
+	import { collapseHeader } from '$lib/stores';
 	import Histograms from './Histograms.svelte';
 	import MetadataHeader from './MetadataHeader.svelte';
 	import Slices from './Slices.svelte';
 	import Tags from './Tags.svelte';
 </script>
 
-<div class="h-full p-5 overflow-y-auto shrink-0 bg-yellowish-light w-[380px]">
+<div
+	class="h-full p-5 overflow-y-auto shrink-0 bg-yellowish-light w-[380px] {$collapseHeader
+		? 'hide-sidebar'
+		: 'show-sidebar'}"
+>
 	<MetadataHeader />
 	<Slices />
 	<Tags />
 	<Histograms />
 </div>
+
+<style>
+	div {
+		transition: all 0.5s ease-in-out;
+	}
+	.show-sidebar {
+		visibility: visible;
+	}
+
+	.hide-sidebar {
+		visibility: none;
+		margin-left: -380px;
+	}
+</style>
