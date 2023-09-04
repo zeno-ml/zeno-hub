28b088776c3754ef24c0b3e37a5b521bc02de3a3
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-04 10:30:11 -0700

    fix: redirect to login page after logout

--- frontend/src/routes/(user)/logout/+page.server.ts
+++ frontend/src/routes/(user)/logout/+page.server.ts
@@ -3,6 +3,6 @@ import { redirect, type Actions } from '@sveltejs/kit';
 export const actions: Actions = {
 	default: async ({ cookies }) => {
 		cookies.delete('loggedIn', { path: '/' });
-		throw redirect(303, '/');
+		throw redirect(303, '/login');
 	}
 };
