1f30e966b4fcab3ea6f2d1beaccfdbcfccd39444
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-19 08:09:46 -0700

    fix: remove stray layout file (#193)

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { browser } from '$app/environment';
-	import { PUBLIC_AMPLITUDE_API_KEY } from '$env/static/public';
+	import { env } from '$env/dynamic/public';
 	import { featureFlags } from '$lib/stores';
 	import { zenoFeatureFlags } from '$lib/util/features';
 	import * as amplitude from '@amplitude/analytics-browser';
@@ -10,8 +10,8 @@
 
 	featureFlags.set({ ...zenoFeatureFlags, ...data.features });
 
-	if (browser && PUBLIC_AMPLITUDE_API_KEY)
-		amplitude.init(PUBLIC_AMPLITUDE_API_KEY, {
+	if (browser && env.PUBLIC_AMPLITUDE_API_KEY)
+		amplitude.init(env.PUBLIC_AMPLITUDE_API_KEY, {
 			defaultTracking: true
 		});
 </script>
