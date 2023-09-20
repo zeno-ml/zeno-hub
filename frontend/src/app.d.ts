d4074f056e73aa8d03d590e602c003663bcf15a5
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-19 21:39:23 -0700

    typescript

--- frontend/src/app.d.ts
+++ frontend/src/app.d.ts
@@ -10,12 +10,3 @@ declare global {
 }
 
 export {};
-
-declare type Item = import('svelte-dnd-action').Item;
-declare type DndEvent<ItemType = Item> = import('svelte-dnd-action').DndEvent<ItemType>;
-declare namespace svelte.JSX {
-	interface HTMLAttributes<T> {
-		onconsider?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-		onfinalize?: (event: CustomEvent<DndEvent<ItemType>> & { target: EventTarget & T }) => void;
-	}
-}
