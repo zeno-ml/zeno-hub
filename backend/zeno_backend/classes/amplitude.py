154d591abfe001b34f71e1fcc17c88ac2896f801
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-25 15:29:50 -0400

    fix: fix singleton amplitude tracking to work w/o key

--- backend/zeno_backend/classes/amplitude.py
+++ backend/zeno_backend/classes/amplitude.py
@@ -8,17 +8,25 @@ class AmplitudeHandler:
     """Class to handle amplitude events.
 
     Attributes:
-        _client (Amplitude | None): amplitude client singleton object.
+        _client (Amplitude | None): amplitude client object.
+        _instance (AmplitudeHandler | None): amplitude handler singleton object.
     """
 
     _client = None
+    _instance = None
 
     def __new__(cls):
         """Create a new amplitude handler if one doesn't exist."""
-        if "AMPLITUDE_API_KEY" in os.environ:
-            if cls._client is None:
-                cls._client = Amplitude(os.environ["AMPLITUDE_API_KEY"])
-        return cls._client
+        if cls._instance is None:
+            cls._instance = super(AmplitudeHandler, cls).__new__(cls)
+        return cls._instance
+
+    def __init__(self):
+        """Initialize the amplitude singleton."""
+        if not hasattr(self, "initialized"):
+            if "AMPLITUDE_API_KEY" in os.environ:
+                self._client = Amplitude(os.environ["AMPLITUDE_API_KEY"])
+            self.initialized = True
 
     def track(self, event: BaseEvent):
         """Track an amplitude event.
