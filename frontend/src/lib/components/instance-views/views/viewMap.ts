9fe5ee096ff4b1870eca6d4b31c61aa7abc9d13c
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-29 12:15:48 -0400

    feat: add space-separated-values (#246)

--- frontend/src/lib/components/instance-views/views/viewMap.ts
+++ frontend/src/lib/components/instance-views/views/viewMap.ts
@@ -7,18 +7,20 @@ import ImageClassification from './views/ImageClassification.svelte';
 import ImageSegmentation from './views/ImageSegmentation.svelte';
 import OpenAiChat from './views/OpenAIChat.svelte';
 import OpenAiChatMarkdown from './views/OpenAIChatMarkdown.svelte';
+import SpaceSeparatedValues from './views/SpaceSeparatedValues.svelte';
 import TextClassification from './views/TextClassification.svelte';
 
 // MUST update /backend/zeno_backend/routers/sdk.py when adding new views
 export const viewMap: Record<string, ComponentType> = {
 	'audio-transcription': AudioTranscription,
-	chatbot: Chatbot,
 	'code-generation': Codegen,
 	'image-classification': ImageClassification,
 	'image-segmentation': ImageSegmentation,
+	'openai-chat-markdown': OpenAiChatMarkdown,
 	'openai-chat': OpenAiChat,
+	'space-separated-values': SpaceSeparatedValues,
 	'text-classification': TextClassification,
-	'openai-chat-markdown': OpenAiChatMarkdown
+	chatbot: Chatbot
 };
 
 export const optionsMap: Record<string, ComponentType> = {
