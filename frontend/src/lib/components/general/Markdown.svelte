ba3e216cb292bba7e3c41680a69ac4cbe058dba1
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-29 14:29:33 -0400

    sizing

--- frontend/src/lib/components/general/Markdown.svelte
+++ frontend/src/lib/components/general/Markdown.svelte
@@ -2,7 +2,9 @@
 	export let renderedText: string;
 </script>
 
-<article class="prose break-all [&_a]:break-all [&_p]:break-all">
+<article
+	class="prose break-all [&_a]:break-all [&_p]:break-all [&_p]:whitespace-pre-wrap max-w-none"
+>
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html renderedText}
 </article>
