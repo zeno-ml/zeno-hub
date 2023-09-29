eef223889e50e70f0bc5eddee3fc2f39aba7ef72
Alex Cabrera <alex.cabrera@gmail.com>
2023-09-29 13:23:51 -0400

    fix: small rendering fixes openai-chat

--- frontend/src/lib/components/general/Markdown.svelte
+++ frontend/src/lib/components/general/Markdown.svelte
@@ -2,7 +2,7 @@
 	export let renderedText: string;
 </script>
 
-<div class="prose">
+<article class="prose break-all [&_a]:break-all [&_p]:break-all">
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html renderedText}
-</div>
+</article>
