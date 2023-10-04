0842cce09cdbe63d791717035d177c2dee2a7e73
Alex Bäuerle <alex@a13x.io>
2023-10-04 14:04:22 +0200

    fix: update chart preferences so that correct slices and metrics are displayed

--- backend/zeno_backend/database/copy.py
+++ backend/zeno_backend/database/copy.py
@@ -1,4 +1,5 @@
 """Functionality for copying instances in the database."""
+import json
 import uuid
 
 from psycopg import sql
@@ -98,16 +99,15 @@ def project_copy(project_uuid: str, copy_spec: ProjectCopy, user: User):
         # Copy data and systems schema.
         db.execute(
             sql.SQL(
-                "INSERT INTO {} (column_id, name, type, data_type) (SELECT "
-                "column_id, name, type, data_type FROM {});"
+                "INSERT INTO {} (column_id, name, type, model, data_type) (SELECT "
+                "column_id, name, type, model, data_type FROM {});"
             ).format(
                 sql.Identifier(f"{new_uuid}_column_map"),
                 sql.Identifier(f"{project_uuid}_column_map"),
             ),
         )
-        copy_data(db, project_uuid, new_uuid)
-
         if not copy_spec.copy_slices:
+            copy_data(db, project_uuid, new_uuid)
             return
 
         # Copy slices.
@@ -120,17 +120,11 @@ def project_copy(project_uuid: str, copy_spec: ProjectCopy, user: User):
         )
 
         if not copy_spec.copy_charts:
+            copy_data(db, project_uuid, new_uuid)
             return
 
-        # Copy charts.
-        db.execute(
-            sql.SQL(
-                "INSERT INTO charts (name, type, parameters, project_uuid) (SELECT "
-                "name, type, parameters, {} as uuid FROM charts "
-                "WHERE project_uuid = %s);"
-            ).format(sql.Literal(new_uuid)),
-            [project_uuid],
-        )
+        copy_charts(db, project_uuid, new_uuid)
+        copy_data(db, project_uuid, new_uuid)
 
 
 def copy_data(db: Database, old_uuid: str, new_uuid: str):
@@ -187,3 +181,123 @@ def copy_data(db: Database, old_uuid: str, new_uuid: str):
             sql.Identifier(col_id),
         )
     )
+    db.commit()
+
+
+def copy_charts(db: Database, old_uuid: str, new_uuid: str):
+    """Copy charts from one project to another.
+
+    Args:
+        db (Database): the database to copy data in.
+        old_uuid (str): uuid of the old project.
+        new_uuid (str): uuid of the new project.
+    """
+    # Copy charts.
+    db.execute(
+        sql.SQL(
+            "INSERT INTO charts (name, type, parameters, project_uuid) (SELECT "
+            "name, type, parameters, {} as uuid FROM charts "
+            "WHERE project_uuid = %s);"
+        ).format(sql.Literal(new_uuid)),
+        [old_uuid],
+    )
+
+    # Get chart parameters.
+    params = db.execute_return(
+        "SELECT id, parameters FROM charts WHERE project_uuid = %s;",
+        [new_uuid],
+    )
+
+    # Update chart slices and metrics.
+    if len(params) > 0:
+        for params_element in params:
+            json_params = json.loads(params_element[1])
+            if "slices" in json_params:
+                json_params["slices"] = list(
+                    map(lambda x: resolve_slice(db, new_uuid, x), json_params["slices"])
+                )
+            if "x_values" in json_params:
+                json_params["x_values"] = list(
+                    map(
+                        lambda x: resolve_slice(db, new_uuid, x)
+                        if isinstance(x, int)
+                        else x,
+                        json_params["y_values"],
+                    )
+                )
+            if "y_values" in json_params:
+                json_params["y_values"] = list(
+                    map(
+                        lambda x: resolve_slice(db, new_uuid, x)
+                        if isinstance(x, int)
+                        else x,
+                        json_params["y_values"],
+                    )
+                )
+            if "metrics" in json_params:
+                json_params["metrics"] = list(
+                    map(
+                        lambda x: resolve_metric(db, new_uuid, x),
+                        json_params["metrics"],
+                    )
+                )
+            if "metric" in json_params:
+                json_params["metric"] = resolve_metric(
+                    db, new_uuid, json_params["metric"]
+                )
+            db.execute(
+                "UPDATE charts SET parameters = %s WHERE id = %s;",
+                [json.dumps(json_params), params_element[0]],
+            )
+
+
+def resolve_slice(db: Database, new_uuid: str, slice_id: int):
+    """Map a slice_id from an old project to a slice_id in a new project.
+
+    Args:
+        db (Database): the database to copy data in.
+        new_uuid (str): uuid of the new project.
+        slice_id (int): id of the slice in the old project.
+    """
+    # All instances slice
+    if slice_id == -1:
+        return slice_id
+
+    slice_name = db.execute_return("SELECT name FROM slices WHERE id = %s;", [slice_id])
+    if len(slice_name) == 0:
+        return None
+    slice_name = slice_name[0][0]
+    new_id = db.execute_return(
+        "SELECT id FROM slices WHERE name = %s AND project_uuid = %s;",
+        [slice_name, new_uuid],
+    )
+    if len(new_id) == 0:
+        return None
+    return new_id[0][0]
+
+
+def resolve_metric(db: Database, new_uuid: str, metric_id: int):
+    """Map a metric_id from an old project to a metric_id in a new project.
+
+    Args:
+        db (Database): the database to copy data in.
+        new_uuid (str): uuid of the new project.
+        metric_id (int): id of the metric in the old project.
+    """
+    # Num_instances metric
+    if metric_id == -1:
+        return metric_id
+
+    metric_name = db.execute_return(
+        "SELECT name FROM metrics WHERE id = %s;", [metric_id]
+    )
+    if len(metric_name) == 0:
+        return None
+    metric_name = metric_name[0][0]
+    new_id = db.execute_return(
+        "SELECT id FROM metrics WHERE name = %s AND project_uuid = %s;",
+        [metric_name, new_uuid],
+    )
+    if len(new_id) == 0:
+        return None
+    return new_id[0][0]
