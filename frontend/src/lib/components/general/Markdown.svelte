e94b484a302ec85a4e0201213a1d79e986baa0f2
Alex Cabrera <alex.cabrera@gmail.com>
2023-10-04 23:59:16 -0700

    fix: word breaks in reports (#270)

--- frontend/src/lib/components/general/Markdown.svelte
+++ frontend/src/lib/components/general/Markdown.svelte
@@ -2,9 +2,7 @@
 	export let renderedText: string;
 </script>
 
-<article
-	class="prose break-all [&_a]:break-all [&_p]:break-all [&_p]:whitespace-pre-wrap max-w-none"
->
+<article class="prose [&_a]:break-word [&_p]:whitespace-pre-wrap max-w-none">
 	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
 	{@html renderedText}
 </article>
