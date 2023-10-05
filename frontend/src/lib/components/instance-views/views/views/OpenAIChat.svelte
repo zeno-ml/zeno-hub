b65a2880ef36245d7eca34fb0b0a7c4d02098640
Alex Cabrera <alex.cabrera@gmail.com>
2023-10-04 23:59:32 -0700

    fix: fix rendering of long markdown (#268)

--- frontend/src/lib/components/instance-views/views/views/OpenAIChat.svelte
+++ frontend/src/lib/components/instance-views/views/views/OpenAIChat.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-	import { resolveDataPoint } from '$lib/util/util';
 	import { mdiChevronUp } from '@mdi/js';
-	import CircularProgress from '@smui/circular-progress/src/CircularProgress.svelte';
 	import AssistantBlock from './openai-chat/AssistantBlock.svelte';
 	import SystemBlock from './openai-chat/SystemBlock.svelte';
 	import UserBlock from './openai-chat/UserBlock.svelte';
@@ -11,13 +9,10 @@
 	export let dataColumn: string;
 	export let labelColumn: string;
 
-	$: showall = false;
-	$: fetchJSON = (async () => {
-		const response = await resolveDataPoint(entry[dataColumn]);
-		const resp = await (response as Response).json();
-		showall = resp.length <= 5;
-		return resp;
-	})();
+	const data = JSON.parse(entry[dataColumn] as string);
+	let showAll = data.length <= 5;
+
+	$: shownData = showAll ? data : data.slice(-4);
 
 	function entryString(
 		value: number | string | boolean | { role: string; content: string }[]
@@ -26,24 +21,21 @@
 	}
 </script>
 
-{#await fetchJSON}
-	<CircularProgress style="height: 32px; width: 32px; margin-right:20px" indeterminate />
-{:then data}
-	{@const entries = showall ? data : data.slice(-4)}
-	<div class="flex flex-col border border-grey-light rounded p-2.5 m-1 w-[32rem]">
-		{#if !showall}
-			<button
-				class="self-center bg-transparent cursor-pointer flex items-center p-1 -mt-1.5 rounded-2xl hover:bg-grey-lighter"
-				on:click={() => (showall = true)}
-			>
-				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 fill-grey-darker">
-					<path d={mdiChevronUp} />
-				</svg>
-				<span class="pr-1">Show All</span>
-			</button>
-		{/if}
-		{#if data}
-			{#each entries as item}
+<div class="flex flex-col border border-grey-light rounded p-2.5 m-1 w-[32rem]">
+	{#if !showAll}
+		<button
+			class="self-center bg-transparent cursor-pointer flex items-center p-1 -mt-1.5 rounded-2xl hover:bg-grey-lighter"
+			on:click={() => (showAll = true)}
+		>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 fill-grey-darker">
+				<path d={mdiChevronUp} />
+			</svg>
+			<span class="pr-1">Show All</span>
+		</button>
+	{/if}
+	{#if shownData}
+		{#key shownData}
+			{#each shownData as item}
 				{#if item['role'] === 'system'}
 					<SystemBlock input={item['content']} />
 				{:else if item['role'] === 'assistant'}
@@ -52,15 +44,15 @@
 					<UserBlock input={item['content']} />
 				{/if}
 			{/each}
-		{/if}
-		{#if entry[modelColumn]}
-			<AssistantBlock input={entryString(entry[modelColumn])} output={true} />
-		{/if}
-		{#if entry[labelColumn]}
-			<div class="flex flex-col -mx-2.5 -mb-2.5 mt-2.5 p-1 border-t border-grey-lighter">
-				<span class="font-medium">Expected:</span>
-				<span>{entry[labelColumn]}</span>
-			</div>
-		{/if}
-	</div>
-{/await}
+		{/key}
+	{/if}
+	{#if entry[modelColumn]}
+		<AssistantBlock input={entryString(entry[modelColumn])} output={true} />
+	{/if}
+	{#if entry[labelColumn]}
+		<div class="flex flex-col -mx-2.5 -mb-2.5 mt-2.5 p-1 border-t border-grey-lighter">
+			<span class="font-medium">Expected:</span>
+			<span>{entry[labelColumn]}</span>
+		</div>
+	{/if}
+</div>
