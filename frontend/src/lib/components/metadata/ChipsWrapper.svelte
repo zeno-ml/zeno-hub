8babfa0a4c33260ea12d8cdd87ae4760f93c6ce4
Alex Bäuerle <alex@a13x.io>
2023-10-11 16:50:28 +0200

    feat: add addslice button to selection predicates (#293)

--- frontend/src/lib/components/metadata/ChipsWrapper.svelte
+++ frontend/src/lib/components/metadata/ChipsWrapper.svelte
@@ -2,6 +2,7 @@
 	import { selectionIds, selectionPredicates, selections, tagIds } from '$lib/stores';
 	import { Join, type FilterPredicate } from '$lib/zenoapi';
 	import Button from '@smui/button';
+	import SlicePopup from '../popups/SlicePopup.svelte';
 	import IdsChip from './chips/IdsChip.svelte';
 	import MetadataChip from './chips/MetadataChip.svelte';
 	import SliceChip from './chips/SliceChip.svelte';
@@ -10,8 +11,13 @@
 	$: filters = Object.entries($selections.metadata)
 		.filter(([, value]) => value.predicates.length > 0)
 		.map(([key, value]) => [key, value.predicates as unknown] as [string, FilterPredicate[]]);
+
+	let showNewSlice = false;
 </script>
 
+{#if showNewSlice}
+	<SlicePopup on:close={() => (showNewSlice = false)} />
+{/if}
 <div class="flex flex-wrap height-fit items-center py-1">
 	{#if $selections.slices.length + $selections.tags.length + filters.length === 0 && $selectionIds === undefined}
 		<p style="margin: 0px">
@@ -50,6 +56,18 @@
 				}}
 			>
 				clear all
+			</Button>
+		{/if}
+		{#if $selectionPredicates !== undefined && $tagIds === undefined && $selectionIds === undefined}
+			<Button
+				variant="outlined"
+				class="ml-2"
+				on:keydown={() => ({})}
+				on:click={() => {
+					showNewSlice = true;
+				}}
+			>
+				create slice
 			</Button>
 		{/if}
 	{/if}
