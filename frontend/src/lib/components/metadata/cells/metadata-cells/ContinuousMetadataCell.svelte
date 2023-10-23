6f7f482b13221098e37f3f05b76913d21e808d36
Alex Cabrera <alex.cabrera@gmail.com>
2023-10-23 13:01:10 -0700

    fix: update continuous filtering (#349)

--- frontend/src/lib/components/metadata/cells/metadata-cells/ContinuousMetadataCell.svelte
+++ frontend/src/lib/components/metadata/cells/metadata-cells/ContinuousMetadataCell.svelte
@@ -17,66 +17,54 @@
 	export let updatePredicates: (predicates: FilterPredicate[]) => void;
 
 	let view: View;
-	let localSelection: number[] = [];
-	let localSelectionX: number[] = [];
+	let timer: ReturnType<typeof setTimeout>;
 
-	function updateSel(filterPreds: FilterPredicate[]) {
-		if (filterPreds.length !== 0) {
-			view.signal('brush', { bucket: localSelection });
-			view.signal('brush_x', localSelectionX);
-		} else {
-			view.signal('brush', {});
+	$: if (view) {
+		view.removeSignalListener('brush', setSelection);
+		if (filterPredicates.length === 0) {
 			view.signal('brush_x', []);
 		}
-		view.runAsync();
+		view.addSignalListener('brush', setSelection);
 	}
 
-	$: if (view) {
-		view.addSignalListener('brush', (...s) => (localSelection = s[1].bucket ? s[1].bucket : []));
-		view.addSignalListener('brush_x', (...s) => (localSelectionX = s[1]));
-	}
-
-	$: if (view) {
-		updateSel(filterPredicates);
-	}
-
-	function setSelection() {
-		if (localSelection.length > 0) {
+	function setSelection(_: string, entry: Record<string, [number, number]>) {
+		clearTimeout(timer);
+		if (entry.bucket === undefined) {
+			updatePredicates([]);
+			return;
+		}
+		const data = entry.bucket;
+		if (isNaN(data[0]) || isNaN(data[1])) {
+			updatePredicates([]);
+			return;
+		}
+		timer = setTimeout(() => {
 			filterPredicates[0] = {
 				column: col,
 				operation: Operation.GTE,
-				value: localSelection[0],
+				value: data[0],
 				join: Join._
 			} as FilterPredicate;
 			filterPredicates[1] = {
 				column: col,
 				operation: Operation.LTE,
-				value: localSelection[1],
+				value: data[1],
 				join: Join.AND
 			} as FilterPredicate;
-		} else {
-			filterPredicates = [];
-		}
-		updatePredicates(filterPredicates);
+			updatePredicates(filterPredicates);
+		}, 100);
 	}
-
-	const endDrag = () => {
-		window.removeEventListener('mouseup', endDrag);
-		setSelection();
-	};
 </script>
 
 <!-- We shallow copy histogram to remove the vega identifiers and force it to update the chart when new data is passed in. -->
-<div id="histogram" on:mousedown={() => window.addEventListener('mouseup', endDrag)}>
-	<VegaLite
-		bind:view
-		spec={continuousVegaSpec($metricRange)}
-		data={{ table: histogram.map((h) => Object.assign({}, h)) }}
-		options={{
-			tooltip: true,
-			actions: false,
-			theme: 'vox',
-			renderer: 'svg'
-		}}
-	/>
-</div>
+<VegaLite
+	bind:view
+	spec={continuousVegaSpec($metricRange)}
+	data={{ table: histogram.map((h) => Object.assign({}, h)) }}
+	options={{
+		tooltip: true,
+		actions: false,
+		theme: 'vox',
+		renderer: 'svg'
+	}}
+/>
