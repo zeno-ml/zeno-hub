7aec9737d9007fa5b2f9ddb61d6f49c7117420e3
Alex Bäuerle <alex@a13x.io>
2023-10-26 18:30:18 +0200

    fix: hotfix beeswarm and radar sizes (#368)

--- frontend/src/lib/components/chart/chart-types/beeswarm-chart/vegaSpec-beeswarm.ts
+++ frontend/src/lib/components/chart/chart-types/beeswarm-chart/vegaSpec-beeswarm.ts
@@ -16,7 +16,6 @@ export default function generateSpec(
 		height: 100,
 		random_id: Date.now(), // used to force re-rendering of the chart
 		padding: { left: 5, right: 5, top: 0, bottom: 20 },
-		autosize: 'fit',
 		signals: [
 			{ name: 'cx', update: 'width / 2' },
 			{ name: 'cy', update: 'height / 2' },
