f01849fdfb01cd1a36c314a15b5d6fb7eb1afafe
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-19 03:48:32 -0800

    fix: route for embed iframe (#454)

--- frontend/src/routes/embed/entry/featured/[numItems=integer]/+page.svelte
+++ frontend/src/routes/embed/entry/featured/[numItems=integer]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import HomeCard from '$lib/components/home/HomeCard.svelte';
+	import type { Project, Report } from '$lib/zenoapi/index.js';
 
 	export let data;
+
+	function getUrl(entry: Report | Project) {
+		return `https://hub.zenoml.com/${'uuid' in entry ? 'project' : 'report'}/${
+			'uuid' in entry ? entry.uuid : entry.id
+		}/${encodeURIComponent(entry.name)}`;
+	}
 </script>
 
 <div class="grid h-full grid-cols-home content-start gap-5 overflow-x-auto">
@@ -9,8 +16,7 @@
 		<div class="relative">
 			<HomeCard entry={entry.entry} stats={entry.stats} user={null} />
 			<a
-				href="https://hub.zenoml.com/{'uuid' in entry.entry ? 'project' : 'report'}/{entry.entry
-					.ownerName}/{entry.entry.name}"
+				href={getUrl(entry.entry)}
 				target="_blank"
 				class="absolute bottom-0 left-0 right-0 top-0 z-20 rounded-lg shadow-sm hover:shadow-md"
 			>
