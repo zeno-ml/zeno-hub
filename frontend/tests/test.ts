a203f0adb66404b8f818c70ff3d4ee746fbdfcdc
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-20 23:36:53 -0800

    env

--- frontend/tests/test.ts
+++ frontend/tests/test.ts
@@ -6,7 +6,7 @@ test('can go to home', async ({ page }) => {
 
 test('can login', async ({ page }) => {
 	await page.goto('/login');
-	await page.fill('input[name="username"]', 'test');
-	await page.fill('input[name="password"]', 'Test1212!');
+	await page.fill('input[name="username"]', process.env.HUB_USERNAME || '');
+	await page.fill('input[name="password"]', process.env.HUB_PASSWORD || '');
 	await page.click('button[type="submit"]');
 });
