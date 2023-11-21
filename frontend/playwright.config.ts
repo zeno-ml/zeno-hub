e59b9279470d8753901eb94fdfd0d9cf81188106
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-20 23:06:43 -0800

    test

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -1,10 +1,9 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config: PlaywrightTestConfig = {
-	webServer: {
-		command: 'npm run build && npm run preview',
-		port: 4173
-	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/
 };
