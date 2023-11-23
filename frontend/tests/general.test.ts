535ea4559509b6fea41e39fcd011efe32c061361
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-23 11:46:01 -0500

    fixes

--- frontend/tests/general.test.ts
+++ frontend/tests/general.test.ts
@@ -1,4 +1,4 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('can go to home', async ({ page }) => {
 	await page.goto('/');
@@ -11,6 +11,5 @@ test('can login', async ({ page }) => {
 	await page.getByLabel('password').fill(process.env.HUB_PASSWORD || '');
 	await page.getByRole('button', { name: 'Login' }).click();
 
-	// Should have profile button with first two letters, in this case 'te' for 'test'
-	await expect(page.getByRole('button', { name: 'te', exact: true })).toBeVisible();
+	await page.waitForURL('/home/test');
 });
