4fc2b0146e58daa37bf3033c20c7bf67c5dca365
Alex Cabrera <alex.cabrera@gmail.com>
2023-12-01 15:27:21 -0800

    feat: cache chart data

--- backend/init.sql
+++ backend/init.sql
@@ -25,6 +25,7 @@ CREATE TABLE charts (
     name text NOT NULL,
     type text NOT NULL,
     parameters text NOT NULL,
+    data jsonb NOT NULL,
     created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
