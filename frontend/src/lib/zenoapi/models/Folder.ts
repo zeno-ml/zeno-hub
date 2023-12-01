c44bdd71e906ae04e6eef585fd318c4c26fc155a
Alex Cabrera <alex.cabrera@gmail.com>
2023-11-30 16:30:01 -0800

    fix

--- frontend/src/lib/zenoapi/models/Folder.ts
+++ frontend/src/lib/zenoapi/models/Folder.ts
@@ -9,8 +9,10 @@
  * Attributes:
  * id (int): the id of the folder.
  * name (str): the name of the folder.
+ * project_uuid (str | None): the uuid of the project the folder belongs to.
  */
 export type Folder = {
 	id: number;
 	name: string;
+	projectUuid?: string | null;
 };
