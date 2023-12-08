9652532ad8a3e68910bd9c503f7fb84cc5b0a39e
Alex Cabrera <alex.cabrera@gmail.com>
2023-12-08 12:24:20 -0800

    fix

--- frontend/src/lib/components/metadata/chips/TagChip.svelte
+++ frontend/src/lib/components/metadata/chips/TagChip.svelte
@@ -24,7 +24,7 @@
 	}
 </script>
 
-<div class="bg-secondary-light mr-1 w-fit rounded px-2.5 py-1">
+<div class="mr-1 w-fit rounded bg-secondary-light px-2.5 py-1">
 	{$tags.find((tag) => tag.id === tagId)?.tagName}
 	<TrailingIcon class="remove material-icons" on:click={cancelClicked}>cancel</TrailingIcon>
 </div>
