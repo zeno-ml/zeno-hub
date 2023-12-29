8ff5185f20064a6ed73f694ac2bb999827e322fb
Alex Bäuerle <alex@a13x.io>
2023-12-29 09:40:12 +0100

    fix: fix positioning and look of chart settings button

---
+++ frontend/src/lib/components/general/CircleIconButton.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { Icon } from '@smui/button';
+
+	export let icon: string;
+	export let positioning: string = '';
+</script>
+
+<button
+	class="{positioning} flex h-8 w-8 cursor-pointer items-center justify-center rounded-full border border-grey-light text-primary transition hover:bg-primary-mid"
+	on:click
+>
+	<Icon tag="svg" viewBox="0 0 24 24" class="w-5 fill-primary">
+		<path d={icon} />
+	</Icon>
+</button>
